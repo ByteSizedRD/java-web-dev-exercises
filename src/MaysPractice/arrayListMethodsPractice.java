a52372c01aa315bd4a89eab25eab75c4c451313d
ByteSizedRD <56002023+ByteSizedRD@users.noreply.github.com>
2019-11-12 19:37:23 -0600

    HashMapPractice added

--- src/MaysPractice/arrayListMethodsPractice.java
+++ src/MaysPractice/arrayListMethodsPractice.java
@@ -8,18 +8,27 @@ public class arrayListMethodsPractice {
     public static void main(String[] args) {
         ArrayList<String> planets = new ArrayList<String>();
 
-        Collections.addAll(planets, "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune");
+        Collections.addAll(planets, "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus",
+                "Neptune");
 
 
         Collections.sort(planets);
         planets.add("Pluto");
         int size = planets.size();
         boolean contains = planets.contains("Earth");
+        int index = planets.indexOf("Earth");
 
 
 
         System.out.println(planets);
         System.out.println("The \"planets\" array list is " + size + " items long.");
-        System.out.println("the \"planets\" arrary list contains the word \"Earth\": " + contains);
+        System.out.println("The \"planets\" array list contains the word \"Earth\": " + contains);
+        System.out.println("The \"planets\" array list contains the word \"Earth\" at " +index+ " index.");
+
+
+        //toArray method returns an Array contains the elements of the ArrayList.  Need to know the size of an array
+        //before you can create it, so need to include the size() method also.
+        String[] planetsArray = new String[planets.size()];
+        planets.toArray(planetsArray);
     }
 }
---
+++ src/MaysPractice/hashMapPractice.java
@@ -0,0 +1,42 @@
+package MaysPractice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class hashMapPractice {
+    public static void main(String[] args) {
+
+        HashMap<String, Double> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish): ");
+
+        do {
+
+            System.out.println("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.println("Grade: ");
+                Double newGrade = input.nextDouble();
+                students.put(newStudent, newGrade);
+
+                input.nextLine();
+
+            }
+
+        } while(!newStudent.equals(""));
+        System.out.println("\nClass Roster");
+        double sum = 0.0;
+
+        for (Map.Entry<String, Double> student : students.entrySet()) {
+            System.out.println(student.getKey() + " (" + student.getValue() + ")");
+            sum += student.getValue();
+        }
+
+        double avg = sum / students.size();
+        System.out.println("Average grade: " + avg);
+    }
+}
