75ff80b90bf066b4ff4ace01b0513d0b64f9f77c
May <megansmithRD@gmail.com>
2019-12-02 18:55:22 -0600

    .

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,5 +1,12 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
 public class CarTest {
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
@@ -7,5 +14,41 @@ public class CarTest {
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
+
+   Car test_car;
+
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+
+    @Test
+    public void emptyTest() {
+        assertEquals(10, 10, .001);
+    }
+
+    @Test
+    public void testInitialGasTank() {
+        assertEquals(10, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(600);
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Should't get here, car cannot have more gas in tank than the size of the tank");
+    }
+
 
 }
